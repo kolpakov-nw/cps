255aff34cfd8a95560e6ae7b098359aaec478137
kolpakov-nw <kolpakov.nw@gmail.com>
2025-07-21 14:40:28 +0300

    fixe12

--- skript.js
+++ skript.js
@@ -32,13 +32,12 @@ const brandsSwiper = new Swiper('.brands__slider.swiper', {
   });
 
   document.addEventListener('DOMContentLoaded', () => {
-    const toggleBtn = document.querySelector('.read__more-btn'); // Кнопка
-    const brendsSlideStatik = document.querySelector('.brends__slide-statik'); // Блок с логотипами
+    const toggleBtn = document.querySelector('.read__more-btn'); 
+    const brendsSlideStatik = document.querySelector('.brends__slide-statik'); 
   
     toggleBtn.addEventListener('click', () => {
-      brendsSlideStatik.classList.toggle('expanded'); // Переключаем класс
+      brendsSlideStatik.classList.toggle('expanded'); 
   
-      // Меняем иконку в кнопке
       const img = toggleBtn.querySelector('img');
   
       if (brendsSlideStatik.classList.contains('expanded')) {
@@ -50,3 +49,50 @@ const brandsSwiper = new Swiper('.brands__slider.swiper', {
       }
     });
   });
+
+  function checkWidthAndOpenMenu() {
+    if (window.innerWidth <= 1120) {
+      burgerMenu.classList.add('burger-menu--open');
+      overlay.classList.add('overlay--visible');
+    } else {
+      burgerMenu.classList.remove('burger-menu--open');
+      overlay.classList.remove('overlay--visible');
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const overlay = document.querySelector('.overlay');
+    const openBtn = document.getElementById('burger-open');
+    const closeBtn = document.getElementById('burger-close');
+
+    function openMenu() {
+      burgerMenu.classList.add('burger-menu--open');
+      overlay.classList.add('overlay--visible');
+    }
+
+    function checkWidthAndOpenMenu() {
+      if (window.innerWidth >= 1120) {
+        burgerMenu.classList.add('burger-menu--open');
+        overlay.classList.add('overlay--visible');
+      } else {
+        burgerMenu.classList.remove('burger-menu--open');
+        overlay.classList.remove('overlay--visible');
+      }
+    }
+
+    function closeMenu() {
+      burgerMenu.classList.remove('burger-menu--open');
+      overlay.classList.remove('overlay--visible');
+    }
+
+    openBtn.addEventListener('click', openMenu);
+    closeBtn.addEventListener('click', closeMenu);
+    overlay.addEventListener('click', closeMenu);
+
+  checkWidthAndOpenMenu();
+  window.addEventListener('resize', checkWidthAndOpenMenu);
+});
+
+
+
